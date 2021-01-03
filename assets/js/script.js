014c5a0d7dd8ff56f3ee15dc7eaa604ac33be7b4
MalekBenJemia <malekbenjemia@gmail.com>
2021-01-02 20:06:26 -0500

    mid progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,24 @@
 // create a variable to hold the time
 var timer = 0;
+var myTimerVar = null;
 
 // create array to hold questions, possible answers and correct answer
 var questionsBank = [];
 
 var questionDataObj1 = {
     question: "Q1",
-    answers : ["A11", "A12", "A13", "A14"],
+    answer1: "A11",
+    answer2: "A12",
+    answer3: "A13", 
+    answer4: "A14",
     correctAnswerIndex: 4
   };
 var questionDataObj2 = {
     question: "Q2",
-    answers : ["A21", "A22", "A23", "A24"],
+    answer1: "A21",
+    answer2: "A22",
+    answer3: "A23", 
+    answer4: "A24",
     correctAnswerIndex: 3
 };
 
@@ -23,7 +30,6 @@ questionsBank.push(questionDataObj2);
 var timerParentZoneEl = document.getElementById("timer-parent-zone");
 var pageContentEl = document.getElementById("page-content");
 
-
 // display the timer
 var timerDisplay = function() {
     var myobj = document.getElementById("timer-zone");
@@ -32,8 +38,14 @@ var timerDisplay = function() {
     var pageTimerEl = document.createElement("span");
     pageTimerEl.id = "timer-zone";
     pageTimerEl.textContent = timer.toString();
-    console.log(pageTimerEl);
     timerParentZoneEl.appendChild(pageTimerEl);
+};
+
+// remove 1 unit to the timer each 60 milliseconds
+var myTimer = function(){
+    timer--;
+    if (timer == 70 || timer == 60 ) {timerDisplay()};
+    if (timer <=0 ) {clearInterval(myTimerVar);};
 };
 
 // upon clicking Start Quiz
@@ -46,6 +58,7 @@ var startQuizButtonHandler = function(event) {
 
     timerDisplay();
     quizzQuestionDisplay();
+    myTimerVar = setInterval(myTimer, 60)
 };
 
 
